3e84835f2e132d65bc226891aca47e010fe322a7
juannietoc <167789166+juannietoc@users.noreply.github.com>
2024-08-28 05:34:33 +0000

    28 de Agosto

--- Ejecutar.java
+++ Ejecutar.java
@@ -23,6 +23,14 @@ public class Ejecutar{
          * Lógicos
          * 
          */
+
+        /********************************************************************* */
+
+        //Creación del objeto
+        Marcador objMarcador1 = new Marcador("edding", "rojo", 1.2, true);
+        System.out.println(objMarcador1.toString());
+        objMarcador1.rayar();
+        System.out.println(objMarcador1.recargar(10.0));
         
     }
 
