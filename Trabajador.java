4109bea813d558cb555c2fdf617821225a83e47d
juannietoc <juan.nietoc@campusucc.edu.co>
2024-09-16 08:35:09 -0500

    hola2

--- Trabajador.java
+++ Trabajador.java
@@ -5,4 +5,23 @@ public class Trabajador {
     private String area;
     
 
+    public Trabajador() {
+
+    }
+
 }
+"java.configuration.runtimes": [
+  {
+    "name": "JavaSE-1.8",
+    "path": "/path/to/jdk-8",
+  },
+  {
+    "name": "JavaSE-11",
+    "path": "/path/to/jdk-11",
+  },
+  {
+    "name": "JavaSE-17",
+    "path": "/path/to/jdk-17",
+    "default": true
+  },
+]
